ab538112f69e0be9cb3efbdf769a9d90595f75d0
YadavRamprakash22 <105532174+YadavRamprakash22@users.noreply.github.com>
2022-05-26 23:31:56 +0530

    Add files via upload

---
+++ upplow.py
@@ -0,0 +1,12 @@
+s=input('ENter a String:')
+def upplow(s):
+    upp=0
+    low=0
+    for i in s:
+        if i.isupper():
+            upp+=1
+        else:
+            low+=1
+    print('no. of uppercase:',upp)
+    print('no. of lowercase:',low)
+upplow(s)
